c3e31c54c35642629cd8d7ae92c3b1bf4f157700
enjoythedarkside <64778833+enjoythedarkside@users.noreply.github.com>
2023-11-09 18:21:15 +0300

    g

--- src/KullaniciGirisi/KullaniciGirisi.java
+++ src/KullaniciGirisi/KullaniciGirisi.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class KullaniciGirisi {
 
 	public static void main(String[] args) {
-		String userName , password;
+		String userName , password, sifirlama, newPassword;
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Kullanici Adi: ");
@@ -17,7 +17,19 @@ public class KullaniciGirisi {
 		if ( userName.equals("Merve") && password.equals("bella123")){
 			System.out.println("Giris basarili");
 		}else {
-			System.out.println("Girilen bilgiler eslesmemektedir");
+			System.out.println("Girilen bilgiler yanlis");
+			System.out.println("Sifrenizi sifirlamak istermisiniz");
+			sifirlama = input.nextLine();
+			if (sifirlama.equals("evet")) {
+				System.out.println("yeni sifrenizi giriniz");
+			}
+			newPassword = input.nextLine();
+			
+			if (newPassword.equals("bella123")) {
+				System.out.println("yeni sifre eskisiyle ayni olamaz");
+			}else {
+				System.out.println("Yeni sifre olusturuldu");
+			}
 		}
 
 	}
